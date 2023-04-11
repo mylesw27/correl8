735364d9e183cb347b17b2044cb84954b2863e33
mylesw27 <myleswiegel27@gmail.com>
2023-04-11 16:31:38 -0700

    update daily response put route

--- controllers/responses.js
+++ controllers/responses.js
@@ -98,7 +98,8 @@ router.post('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     const foundDaily = await db.daily.findOne({
         where: {
-            id: req.params.id
+            id: req.params.id,
+            userId: res.locals.user.id
         }
     })
     const habits = await db.habit.findAll({
@@ -112,12 +113,85 @@ router.get('/:id', async (req, res) => {
             }
         }]
     })
-    console.log(habits.habresponse)
     res.render('responses/show.ejs', {foundDaily, habits})
 })
 
-router.put('/:id', (req, res) => {
-    res.send('Update an existing record of a day')
+router.put('/:id', async (req, res) => {
+    try {
+        const updateDay = await db.daily.findOne({
+            where: {
+                id: req.params.id,
+            }
+        })
+        updateDay.update({
+            rating: req.body.rating,
+            mood_AM: req.body.mood_am,
+            mood_PM: req.body.mood_pm,
+            stress: req.body.stress,
+            activity: req.body.activity,
+            diet: req.body.diet,
+            woke_up: req.body.woke_up,
+            bed: req.body.bed,
+            water: req.body.water,
+            affirmations: req.body.affirmations,
+            notes: req.body.notes,
+        })
+        updateDay.save()
+        await console.log(updateDay.rating)
+    //     const foundHabresponse = await db.user.findOrCreate({
+    //         where: {
+    //             dailyId: req.params.id
+    //         }
+    //     })
+    //     const foundHabits = await db.habit.findAll({
+    //     where: {
+    //         userId: res.locals.user.id
+    //     }
+    // })
+    // const responseArray = Object.entries(req.body)
+
+    // let habitArray = []
+    // responseArray.forEach((response, i) => {
+    //     let splitResponse = response[0].split("_")
+    //     if (splitResponse[0] === "habit" ) {
+    //         habitArray.push(splitResponse)
+    //     }
+        
+    // })
+    // let yesHabits = habitArray.map(habit => Number(habit[1]))
+    
+    // foundHabits.forEach( async (taco,i) => {
+    //     console.log(`Yes habits: ${yesHabits} foundHabitsID: ${taco.id}`)
+    //     if (yesHabits.includes(taco.id)) {
+    //         await db.habresponse.findOrCreate({
+    //             where: {
+    //                 date: req.body.date,
+    //                 userId: res.locals.user.id,
+    //                 habitId: taco.id,
+    //             }, defaults: {
+    //                 response: true,
+    //                 dailyId: newDay.id
+    //             }
+    //         })
+    //     } else {
+    //         await db.habresponse.findOrCreate({
+    //             where: {
+    //                 date: req.body.date,
+    //                 userId: res.locals.user.id,
+    //                 habitId: taco.id,
+    //             }, defaults: {
+    //                 response: false,
+    //                 dailyId: newDay.id
+    //             }
+    //         })
+
+    //     }
+    // })
+    } catch(error) {
+        console.log(error)
+    }
+
+    res.redirect('/responses')
 })
 
 router.delete('/', (req, res) => {
